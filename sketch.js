3625c9d0ff8a9eace46b882ec753fb9520e554ce
Devin Lane <devin.c.lane@gmail.com>
2022-07-15 16:27:35 -0700

    average-color-columns initial

--- sketch.js
+++ sketch.js
@@ -2,11 +2,20 @@
 // get shadows or contours or any data from the image
 // Convert that into frequency
 
-// document.getElementById('start').addEventListener('click', async () => {
-// 	await Tone.start()
-// 	console.log('audio is ready')
-// })
+let ready = false;
 
+document.getElementById('start').addEventListener('click', async () => {
+	i = 0;
+    ready = true;
+    clear();
+    await Tone.start()
+	console.log('audio is ready')
+})
+
+// create an average
+const average = array => {
+    return array.reduce((a, b) => a + b) / array.length;
+}
 
 // from https://p5js.org/reference/#/p5.Image/get
 let myImage;
@@ -19,7 +28,8 @@ function preload() {
 
 function setup() {
     createCanvas(1000, 1000);
-    background(myImage);
+    frameRate(10);
+    // background(myImage);
     // noStroke();
 // todo: loop across the image size
 // send this data to tone.js
@@ -30,15 +40,37 @@ y Number: y-coordinate of the pixel
 w Number: width
 h Number: height 
 */
-    // c = myImage.get(60, 90);
-    // console.log(c)
+
     // synth.triggerAttackRelease(c[0], '32n');
 }
 
-function mousePressed() {
-    colorData = myImage.get(mouseX, mouseY)
-    synth.triggerAttackRelease(colorData[0], '16n')
-    console.log(colorData)
-}
+// function mousePressed() {
+//     colorData = myImage.get(mouseX, mouseY)
+//     synth.triggerAttackRelease(colorData[0], '16n')
+//     console.log(colorData)
+// }
 
-//get() returns color here
+let i = 0;
+function draw() {
+    if (!ready) {
+        return;
+    }
+    if (i >= myImage.width) {
+        ready = false;
+        return;
+    }
+    let imageColumn = myImage.get(i, 0, 25, myImage.height);
+    image(imageColumn, i, 0)
+    imageColumn.loadPixels();
+
+    // linear interpolation to get a range of pitches I like
+    let pitch = lerp(60, 900, average(imageColumn.pixels)/255)
+
+    const now = Tone.now()
+    // trigger the attack immediately
+    console.log('ok')
+    synth.triggerAttackRelease(pitch, '16n', now)
+    // wait one second before triggering the release
+    // synth.triggerAttackRelease(pitch, '16n')
+    i++
+}
